29099429f24954c8bf9d7f77e3e95f9e5779a735
Bakaryc50 <bakaryc50@gmail.com>
2021-09-06 17:48:15 -0700

    Adding commit: 0x00-python-hello_world/102-magic_calculation.py

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def magic_calculation(a,b):
+    """
+        3     0 LOAD_CONST               1 (98)
+              3 LOAD_FAST                0 (a)
+              6 LOAD_FAST                1 (b)
+              9 BINARY_POWER
+             10 BINARY_ADD
+             11 RETURN_VALUE
+    """
