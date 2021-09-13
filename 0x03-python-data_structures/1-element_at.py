fe4ff5036d184e8c61a2a1b09e8321403d0f153e
Bakaryc50 <bakaryc50@gmail.com>
2021-09-13 13:37:14 -0700

    Adding fixed issue commit: 0x03-python-data_structures/1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -9,8 +9,6 @@ def element_at(my_list, idx):
     idx: the index
     Return: the element
     """
-    if my_list:
-        if idx < 0 or idx > len(my_list):
-            return None
-        else:
-            return my_list[idx]
+    if my_list is not None and -1 < idx < len(my_list):
+        return my_list[idx]
+    return None
