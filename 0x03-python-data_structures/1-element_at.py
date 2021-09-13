2074c9269e667180bca3b7f0f5e15dfab1993ab5
Bakaryc50 <bakaryc50@gmail.com>
2021-09-13 12:54:44 -0700

    Adding commit: 0x03-python-data_structures/1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    """
+    Function that retrieves an element from a list
+    Parameters:
+    my_list: the list
+    idx: the index
+    Return: the element
+    """
+    if my_list:
+        if idx < 0 or idx > len(my_list):
+            print("None")
+        else:
+            return my_list[idx]
