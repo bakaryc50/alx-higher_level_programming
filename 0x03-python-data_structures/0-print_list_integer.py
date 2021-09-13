5779369a83f925c6f4ce47ee076cfd5df3c488f3
Bakaryc50 <bakaryc50@gmail.com>
2021-09-13 12:01:18 -0700

    Adding commit: 0x03-python-data_structures/0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -6,5 +6,5 @@ def print_list_integer(my_list=[]):
     print_list_integer - prints all integers of a list
     Parameter: my_list0-print_list_integer.py
     """
-
-    print('\n'.join(['{:d}'.format(i) for i in my_list]))
+    if my_list:
+        print('\n'.join(['{:d}'.format(i) for i in my_list]))
