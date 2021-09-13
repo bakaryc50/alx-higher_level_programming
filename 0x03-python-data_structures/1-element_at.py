ae63de9baa50d7ccb0d20e75293ed6d7128b9cb9
Bakaryc50 <bakaryc50@gmail.com>
2021-09-13 13:13:05 -0700

    Adding commit: 0x03-python-data_structures/1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -11,6 +11,6 @@ def element_at(my_list, idx):
     """
     if my_list:
         if idx < 0 or idx > len(my_list):
-            return "None"
+            return None
         else:
             return my_list[idx]
