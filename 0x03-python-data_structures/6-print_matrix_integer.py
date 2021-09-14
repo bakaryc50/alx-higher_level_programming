442c5cfcd62a017ecf907d27129afde546fed42b
Bakaryc50 <bakaryc50@gmail.com>
2021-09-13 17:40:25 -0700

    Adding commit: 0x03-python-data_structures/6-print_matrix_integer

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    """
+    Function that prints a matrix of integers
+    Parameters:
+    @matrix: the matrix
+    """
+    if matrix is not None:
+        for i in matrix:
+            for j in i:
+                print("{} {}".format(i, j))
