bdefcae26e6bacee800ad654fe1586001040e0a0
Bakaryc50 <bakaryc50@gmail.com>
2021-09-15 18:17:45 -0700

    Adding commit: 0x04-python-more_data_structures/9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -6,5 +6,5 @@ def multiply_by_2(a_dictionary):
     function that returns a new dictionary with all values * by 2.
     """
     if a_dictionary is not None:
-        return list(map(lambda val: val * 2, a_dictionary))
+        return {key: val * 2 for key, val in a_dictionary.items()}
     return a_dictionary
