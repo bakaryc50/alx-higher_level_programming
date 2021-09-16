2f0d6abb874594164ddd08d3e51acdbeb7f4f3ad
Bakaryc50 <bakaryc50@gmail.com>
2021-09-15 18:31:35 -0700

    Adding commit: 0x04-python-more_data_structures/10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,6 +7,6 @@ def best_score(a_dictionary):
     """
     if a_dictionary is not None:
         return max(a_dictionary.items(),
-                key=lambda item: item[1], default[None]
+                key=lambda item: item[1], default=[None]
                )[0]
     return None
