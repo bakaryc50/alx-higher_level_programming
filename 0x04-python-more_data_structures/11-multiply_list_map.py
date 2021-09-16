b06bb656106240cbbab1716326bcd893d23820cb
Bakaryc50 <bakaryc50@gmail.com>
2021-09-15 18:45:23 -0700

    Adding commit: 0x04-python-more_data_structures/11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -6,7 +6,5 @@ def multiply_list_map(my_list=[], number=0):
     function that returns a list with all values multi by a number.
     """
     if my_list is not None:
-        return list(map(
-            lambda val: val * number, my_list
-               ))
-    return my_list
+        return list(map(lambda val: val * number, my_list))
+    return none
