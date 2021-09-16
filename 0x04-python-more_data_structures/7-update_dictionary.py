de1f8248a1edb141cbf5639cd7c773bd43373e55
Bakaryc50 <bakaryc50@gmail.com>
2021-09-15 17:46:46 -0700

    Adding commit: 0x04-python-more_data_structures/7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    function that replaces or adds key/value in a dictionary.
+    """
+    if a_dictionary is not None:
+        a_dictionary[key] = value
+    return None
