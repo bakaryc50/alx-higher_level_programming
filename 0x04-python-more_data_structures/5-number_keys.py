7a3674fd5f00968c0e2e8d6e8af675a1d52e25b6
Bakaryc50 <bakaryc50@gmail.com>
2021-09-15 17:18:22 -0700

    Adding commit: 0x04-python-more_data_structures/5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary):
+    """
+    function that returns the number of keys in a dictionary.
+    """
+    if a_dictionary is not None:
+        return len(a_dictionary.keys())
+    return None
