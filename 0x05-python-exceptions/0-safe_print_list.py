a61a737cbfd84cf04ed0894241c228d1001ed21e
Bakaryc50 <bakaryc50@gmail.com>
2021-09-27 16:37:04 -0700

    Adding commit fixed white space: 0x05-python-exceptions/0-safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     Prints x elements of a list
     @my_list: the list
     """
-    try: 
+    try:
         for i in range(x):
             print(my_list[i], end="")
         return x
