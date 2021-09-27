fcc143b2c1d68ea97543debb54fd02b67e63861d
Bakaryc50 <bakaryc50@gmail.com>
2021-09-27 16:34:58 -0700

    Adding commit: 0x05-python-exceptions/0-safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,15 +6,11 @@ def safe_print_list(my_list=[], x=0):
     Prints x elements of a list
     @my_list: the list
     """
-    try:
-        
+    try: 
         for i in range(x):
             print(my_list[i], end="")
-        
         return x
-    
     except IndexError:
         return i
-    
     finally:
         print()
