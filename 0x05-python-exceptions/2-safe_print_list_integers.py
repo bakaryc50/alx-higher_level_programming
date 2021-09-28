15c098c88a6ae1ccdcb3d0649902fba233c248e2
Bakaryc50 <bakaryc50@gmail.com>
2021-09-27 17:09:49 -0700

    Adding commit fixed new line: 0x05-python-exceptions/0-safe_print_list_integer.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,9 +10,7 @@ def safe_print_list_integers(my_list=[], x=0):
         for i in range(x):
             print("{:d}".format(my_list[i]), end="")
         return x
-    except ValueError:
-        print("not an integer value")
-    except IndexError:
-        print("IndexError: list index out of range")
+    except (ValueError, IndexError, TypeError):
+        pass
     finally:
         print()
