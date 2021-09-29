a21ca445709f4981c8ad5686dd3845d53fcf56d7
Bakaryc50 <bakaryc50@gmail.com>
2021-09-29 16:48:11 -0700

    Adding commit fixing 2 blank lines: 0x06-python-classes/1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+""" Module providing a definition of a class 'Square'
+"""
+
+
+class Square():
+    """ A class 'Square' that defines a square by based on 0-square
+    """
+    __size
