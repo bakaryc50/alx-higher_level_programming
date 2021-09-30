9d48b517f0aa4c53566af8e75602a3f00725fb09
Bakaryc50 <bakaryc50@gmail.com>
2021-09-29 17:43:08 -0700

    Adding commit fixing bug: 0x06-python-classes/4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,21 +11,23 @@ class Square():
     def __init__(self, size=0):
         """ Instantiation of a Square or initialization
         """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get the size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the Square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-    def size(self):
-        """Accessing and retrieving the size of the Squre
-        """
-        return self.__size
-
-    def size(self, value):
-        """Setting and updating the size of the Square
-        """
-        self.__size = value
 
     def area(self):
         """ Compute the area of the Square
