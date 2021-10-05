ddbd8a8e337787fbc72b3a1fd9d3779968eb0e3a
Bakaryc50 <bakaryc50@gmail.com>
2021-10-04 17:28:13 -0700

    Fixing correct output commit: 0x08-python-more_classes/1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,17 +12,8 @@ class Rectangle():
     def __init__(self, width=0, height=0):
         """ A class instantiation or initialization
         """
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        type(self).width = width
-
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        type(self).height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -34,11 +25,11 @@ class Rectangle():
     def width(self, value):
         """A setter method
         """
-        if not isinstance(width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        type(self).__width = width
+        type(self).__width = value
 
     @property
     def height(self):
@@ -50,8 +41,8 @@ class Rectangle():
     def height(self, value):
         """A setter method
         """
-        if not isinstance(width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        type(self).__height = height
+        type(self).__height = value
