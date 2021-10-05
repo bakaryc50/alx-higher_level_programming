78bc1255a731fc8fe95b0e7cb1d38fc2ee5d5ca9
Bakaryc50 <bakaryc50@gmail.com>
2021-10-04 17:40:23 -0700

    Fixing type value error: 0x08-python-more_classes/1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -41,8 +41,8 @@ class Rectangle():
     def height(self, value):
         """A setter method
         """
-        if not isinstance(type(value), int):
-            raise TypeError("width must be an integer")
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
