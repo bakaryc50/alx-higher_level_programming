5f00770a3134f7666762da81d43e4694294d4285
Bakaryc50 <bakaryc50@gmail.com>
2021-10-13 15:48:28 -0700

    Adding commit: 0x0B-python-input_output/0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Reading a text file module in utf-8
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file and prints it to stdout
+    """
+    with open(filename, encoding="UTF-8") as file:
+        print(file.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+# The Readme file 0x0B-python-input_output
