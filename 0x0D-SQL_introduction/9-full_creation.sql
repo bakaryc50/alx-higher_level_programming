adeb6551c244f2bbd422e675a845597fae226dff
Bakaryc50 <bakaryc50@gmail.com>
2021-12-17 11:27:45 -0800

    Bakary commit:

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- Full creation of second_table
+-- Full creation of second_table including creation and insertion
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
