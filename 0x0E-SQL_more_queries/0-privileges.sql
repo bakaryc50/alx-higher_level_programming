ffcb3057716de350f07d976aa5f5728bac390e8f
Bakaryc50 <bakaryc50@gmail.com>
2021-12-18 12:00:13 -0800

    Bakary commit: 0-privileges.sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Show all privileges of the user user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- Show all privileges of the user user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+# The Readme file *0x0D-SQL_introduction* 
