0eaf4aeee7a95bd0e512ebbfcbeb289031176979
Bakaryc50 <bakaryc50@gmail.com>
2021-12-18 12:29:46 -0800

    Bakary commit: 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- create the user user_0d_1 with password user_0d_1_pwd.
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY "user_0d_1_pwd";
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 -- Grants all privileges to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1_pwd@localhost;
