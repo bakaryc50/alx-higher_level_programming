18aaaaa8670c903d9a8941c87e2bcd96e8fd2bcb
Bakaryc50 <bakaryc50@gmail.com>
2021-12-19 10:11:35 -0800

    Bakary commit:

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,4 @@
--- Lists all genres and displays the number of shows linked to each
+-- Lists all Comedy
 SELECT ts.title 
   FROM tv_genres tg, tv_show_genres tsg, tv_shows ts
  WHERE tg.id = tsg.genre_id AND tsg.show_id = ts.id
