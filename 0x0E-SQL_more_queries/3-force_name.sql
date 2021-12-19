516cec59ac97fcc1a81e6bab8f794f47bd1506cd
Bakaryc50 <bakaryc50@gmail.com>
2021-12-19 06:07:33 -0800

    Bakary commit: 3-force_name.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,5 @@
--- Use database hbtn_0d_2
-USE hbtn_0d_2;
 -- Create the table force_name
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT, 
+	name VARCHAR(256) NOT NULL
+);
