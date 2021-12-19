a9d032494e5e572e737eff12215411455ee91138
Bakaryc50 <bakaryc50@gmail.com>
2021-12-19 06:40:08 -0800

    Bakary commit: 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,9 +1,8 @@
 -- Create the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- Use the database hbtn_Od_usa
+-- Use the database hbtn_0d_usa
 USE hbtn_0d_usa;
 -- Create states Table
-
 CREATE TABLE IF NOT EXISTS states (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
