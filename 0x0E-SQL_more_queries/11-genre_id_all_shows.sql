d943c7486e7d907fe3317f82482779b9085c8c1e
Bakaryc50 <bakaryc50@gmail.com>
2021-12-19 09:12:52 -0800

    Bakary commit: 11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Lists all shows in hbtn_0d_tvshows
+SELECT ts.title, tsg.genre_id
+  FROM tv_shows ts
+  LEFT JOIN tv_show_genres tsg
+ ON ts.id = tsg.show_id
+ ORDER BY ts.title, tsg.genre_id ASC;
