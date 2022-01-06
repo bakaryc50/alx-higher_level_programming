3682d192f4abde7e611438fd44e62f8dd732a2bb
Bakaryc50 <bakaryc50@gmail.com>
2022-01-06 13:52:20 -0800

    Bakary commit : 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
-"""
-List all states starting with 'N' from a MySQL db on localhost at port 3306
-"""
+'''script for task 1'''
 
-from mysqlman import MySQLMan
-from MySQLdb import Error
-from sys import argv, exit, stderr
+import MySQLdb
+import sys
 
 
-HELP = '{} username password database'.format(argv[0])
-HOST = 'localhost'
-PORT = 3306
+def list_N():
+    '''lists all states with a name that starts with N'''
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = 'localhost'
+    port = 3306
+
+    db = MySQLdb.connect(host=host, user=username, passwd=password,
+                         db=db_name, port=port)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states WHERE name regexp "^N.*" ' +
+                'ORDER BY states.id ASC')
+    result = cur.fetchall()
+    cur.close()
+    db.close()
+    if result:
+        for row in result:
+            if row[1][0] == "N":
+                print(row)
 
 
-if __name__ == '__main__':
-    try:
-        mysqlman = MySQLMan(
-            connect=True,
-            user=argv[1],
-            password=argv[2],
-            database=argv[3],
-            host=HOST,
-            port=PORT,
-        )
-    except IndexError:
-        stderr.write('usage: {}\n'.format(HELP))
-        exit(2)
-    except Error as e:
-        stderr.write('{}\n'.format(e.args[1]))
-        exit(1)
-    query = "SELECT id, name FROM states ORDER BY id;"
-    results = mysqlman.query([query, ()])
-    for row in results[0]:
-        if row[1].startswith('N'):
-            print(row)
+if __name__ == "__main__":
+    list_N()
