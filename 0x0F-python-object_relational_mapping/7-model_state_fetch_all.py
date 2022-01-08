295626b10952c33022d77a25514692ab5d6e2157
Bakaryc50 <bakaryc50@gmail.com>
2022-01-08 06:08:50 -0800

    Bakary commit: 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,38 +1,24 @@
 #!/usr/bin/python3
-"""
-List all State objects from the states table of a MySQL database
-"""
+'''script  for task 7'''
 
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv, exit, stderr
+import sys
 
-
-HELP = '{} username password database'.format(argv[0])
-HOST = 'localhost'
-PORT = 3306
-URLFORMAT = '{dialect}+{driver}://{user}:{password}@{host}/{database}'
-
-
-if __name__ == '__main__':
-    try:
-        params = {
-            'dialect': 'mysql',
-            'driver': 'mysqldb',
-            'user': argv[1],
-            'password': argv[2],
-            'host': HOST,
-            'database': argv[3],
-        }
-    except IndexError:
-        stderr.write('usage: {}\n'.format(HELP))
-        exit(2)
-    engine = create_engine(URLFORMAT.format(**params), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = 'localhost'
+    port = '3306'
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+             username, password, host, port, db_name), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-    session = Session()
-    results = session.query(State).order_by(State.id).all()
-    for state in results:
-        print('{}: {}'.format(state.id, state.name))
-    session.close()
+    local_session = Session()
+    states = local_session.query(State).order_by(State.id).all()
+    local_session.close()
+    engine.dispose()
+
+    for state in states:
+        print(str(state.id) + ': ' + state.name)
