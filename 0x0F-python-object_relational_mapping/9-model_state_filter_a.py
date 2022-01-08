e9909b1f1636382c2d2d73e8e5c590ac7ba0fec6
Bakaryc50 <bakaryc50@gmail.com>
2022-01-08 06:30:19 -0800

    Bakary commit: 9-model_state_filter_a.py

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''script  for task '''
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = 'localhost'
+    port = '3306'
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+             username, password, host, port, db_name), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states_a = session.query(State).filter(
+            State.name.contains("a")
+            ).order_by(State.id).all()
+    session.close()
+    engine.dispose()
+
+    for state in states_a:
+        print("{}: {}".format(state.id, state.name))
