3a0b7910d33add19170f725042f487b6a186d400
Bakaryc50 <bakaryc50@gmail.com>
2022-03-27 11:36:46 -0700

    Bakary commit: 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// script that prints 3 lines
+const myLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < myLines.length; i++) {
+  console.log(myLines[i]);
+}
