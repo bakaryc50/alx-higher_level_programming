fd3d1cf79cdb57bcc41df0c9e080f7353b8a1c84
Bakaryc50 <bakaryc50@gmail.com>
2022-03-27 10:06:29 -0700

    Bakary commit: 1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that prints 3 lines
+let myVar = 'C is fun';
+console.log(myVar);
+myVar = 'Python is cool';
+console.log(myVar);
+myVar = 'JavaScript is amazing';
+console.log(myVar);
