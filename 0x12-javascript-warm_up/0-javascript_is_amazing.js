7e8eaf69db3ece72f8eccb43889a54f73d05cc9d
Bakaryc50 <bakaryc50@gmail.com>
2022-03-27 09:41:26 -0700

    Bakary commit: 0-javascript_is_amazing.js

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// script that create a constant variable and prints “JavaScript is amazing”
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,1 @@
+# The README file for **0x12-javascript-warm_up** project
